1b20c5b8927f4ad9ea9eb070b576e920507ed37a
Anca <negulescuanca11@gmail.com>
2024-09-03 14:58:41 +0200

    Add some constants. Colors, API base url & endpoint

---
+++ src/config/api.ts
@@ -0,0 +1,6 @@
+export const API_BASE_URL = 'https://darkroastedbeans.coffeeit.nl';
+
+export const API_ENDPOINTS = {
+	GET_COFFEE_DATA: (id: string) => `/coffee-machine/${id}`,
+};
+export const COFFEE_MACHINE_ID = '60ba1ab72e35f2d9c786c610';
---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+	black: '#000000',
+	white: '#FFFFFF',
+
+	primaryBackgroundColor: '#AED7A0',
+	primaryTextColor: '#FFFFFF',
+	secondaryBackgroundColor: '#9BC88B',
+	secondaryTextColor: '#FFFFFF',
+};
